eb56bf49efdea6d7b6abd6145ee9919e9d391f06
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2014-01-28 15:19:45 +0200

    [sailfish-browser] Add MPL v2.0 license

--- src/link.h
+++ src/link.h
@@ -1,3 +1,14 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Jolla Ltd.
+** Contact: Petri M. Gerdt <petri.gerdt@jollamobile.com>
+**
+****************************************************************************/
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef LINK_H
 #define LINK_H
 
--- tests/auto/tst_historyjs.qml
+++ tests/auto/tst_historyjs.qml
@@ -5,6 +5,10 @@
 **
 ****************************************************************************/
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import QtQuickTest 1.0
 import QtQuick 2.0
 import "/usr/share/sailfish-browser/pages/history.js" as History
