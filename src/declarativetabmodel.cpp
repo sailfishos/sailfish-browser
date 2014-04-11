sailfishos/sailfish-browser

commit cff5aa5683bf250b4e1ce5911297822bd0bdeeb4

[sailfish-browser] Don't active first tab when tabs become available
The first tab is activated by onReadyToLoad handler
of DeclarativeWebContainer.

src/declarativetabmodel.cpp
@@ -341,7 +341,7 @@ void DeclarativeTabModel::tabsAvailable(QList<Tab> tabs)
         }
         const Tab &activeTab = m_tabs.at(index);
         m_tabs.removeAt(index);
-        updateActiveTab(activeTab);
+        m_activeTab = activeTab;
     }
 
     qSort(m_tabs.begin(), m_tabs.end(), DeclarativeTabModel::tabSort);

src/declarativewebcontainer.cpp
@@ -371,6 +371,11 @@ void DeclarativeWebContainer::captureScreen()
     }
 }
 
+void DeclarativeWebContainer::dumpPages() const
+{
+    m_webPages->dumpPages();
+}
+
 void DeclarativeWebContainer::resetHeight(bool respectContentHeight)
 {
     if (!m_webPage || !m_webPage->state().isEmpty()) {
@@ -484,10 +489,10 @@ void DeclarativeWebContainer::onActiveTabChanged(int oldTabId, int activeTabId)
         setThumbnailPath("");
         return;
     }
+    const Tab &tab = m_model->activeTab();
 #ifdef DEBUG_LOGS
     qDebug() << "canGoBack = " << m_canGoBack << "canGoForward = " << m_canGoForward << &tab;
 #endif
-    const Tab &tab = m_model->activeTab();
 
     if (m_canGoForward != (tab.nextLink() > 0)) {
         m_canGoForward = tab.nextLink() > 0;

src/declarativewebcontainer.h
@@ -108,6 +108,7 @@ public:
     Q_INVOKABLE bool activatePage(int tabId, bool force = false);
 
     Q_INVOKABLE void captureScreen();
+    Q_INVOKABLE void dumpPages() const;
 
 signals:
     void contentItemChanged();

src/webpages.h
@@ -45,6 +45,7 @@ public:
     WebPageActivationData page(int tabId, int parentId = 0);
     void release(int tabId, bool virtualize = false);
     int parentTabId(int tabId) const;
+    void dumpPages() const;
 
 private:
     struct WebPageEntry {
@@ -56,7 +57,6 @@ private:
     };
 
     void updateActivePage(WebPageEntry *webPageEntry, bool resurrect);
-    void dumpPages() const;
 
     QPointer<DeclarativeWebContainer> m_webContainer;
     QPointer<QQmlComponent> m_webPageComponent;

commit 919995242d8f08e9cef3a56faf233f61c36ed19f

[sailfish-browser] Fix broken initial tab order
Fix commit fixes the issue caused by commit cff5aa5683b

src/declarativetabmodel.cpp
@@ -315,21 +315,13 @@ void DeclarativeTabModel::tabsAvailable(QList<Tab> tabs)
     m_tabs.clear();
     m_tabs = tabs;
 
-    int activeTabId = loadTabOrder();
     if (m_tabs.count() > 0) {
-        Tab tab;
-        tab.setTabId(activeTabId);
-
-        int index = m_tabs.indexOf(tab);
-        if (index == -1) {
-            index = 0;
-        }
-        const Tab &activeTab = m_tabs.at(index);
-        m_tabs.removeAt(index);
-        m_activeTab = activeTab;
+        loadTabOrder();
+        qSort(m_tabs.begin(), m_tabs.end(), DeclarativeTabModel::tabSort);
+        m_activeTab = m_tabs.at(0);
+        m_tabs.removeAt(0);
     }
 
-    qSort(m_tabs.begin(), m_tabs.end(), DeclarativeTabModel::tabSort);
     endResetModel();
 
     if (count() != oldCount) {
@@ -446,6 +438,10 @@ int DeclarativeTabModel::findTabIndex(int tabId) const
 void DeclarativeTabModel::saveTabOrder()
 {
     QString tabOrder = "";
+    if (m_activeTab.isValid()) {
+        tabOrder = QString("%1,").arg(m_activeTab.tabId());
+    }
+
     for (int i = 0; i < m_tabs.count(); ++i) {
         const Tab &tab = m_tabs.at(i);
         tabOrder.append(QString("%1,").arg(tab.tabId()));
@@ -454,10 +450,9 @@ void DeclarativeTabModel::saveTabOrder()
 #endif
     }
     DBManager::instance()->saveSetting("tabOrder", tabOrder);
-    DBManager::instance()->saveSetting("activeTab", QString("%1").arg(m_activeTab.tabId()));
 }
 
-int DeclarativeTabModel::loadTabOrder()
+void DeclarativeTabModel::loadTabOrder()
 {
     QString tabOrder = DBManager::instance()->getSetting("tabOrder");
     QStringList tmpList = tabOrder.split(",");
@@ -468,17 +463,6 @@ int DeclarativeTabModel::loadTabOrder()
         if (ok) {
             s_tabOrder << tabId;
         }
-    }
-
-    QString activeTab = DBManager::instance()->getSetting("activeTab");
-    int activeTabId = activeTab.toInt(&ok);
-#ifdef DEBUG_LOGS
-    qDebug() << "loaded tab order:" << s_tabOrder << "active tab: " << activeTabId;
-#endif
-    if (ok) {
-        return activeTabId;
-    } else {
-        return 0;
     }
 }
 

src/declarativetabmodel.h
@@ -132,7 +132,7 @@ private:
     void removeTab(int tabId, const QString &thumbnail, int index = -1);
     int findTabIndex(int tabId) const;
     void saveTabOrder();
-    int loadTabOrder();
+    void loadTabOrder();
     void updateActiveTab(const Tab &activeTab);
     void updateTabUrl(int tabId, bool activeTab, const QString &url, bool navigate);
 

src/declarativewebcontainer.cpp
@@ -527,7 +527,7 @@ void DeclarativeWebContainer::screenCaptureReady()
 {
     ScreenCapture capture = m_screenCapturer.result();
 #ifdef DEBUG_LOGS
-    qDebug() << capture.tabId << capture.path << capture.url;
+    qDebug() << capture.tabId << capture.path << capture.url << isActiveTab(capture.tabId);
 #endif
     if (capture.tabId != -1) {
         // Update immediately without dbworker round trip.
@@ -554,10 +554,13 @@ void DeclarativeWebContainer::triggerLoad()
 
 void DeclarativeWebContainer::onModelLoaded()
 {
-    // Load placeholder when no pages exist. If a page exists,
-    // it gets initialized by onActiveTabChanged.
-    if (!webPage()) {
+    // This signal handler is responsible for activating
+    // the first page.
+    if (m_model->hasNewTabData() || m_model->count() == 0) {
         activatePage(m_model->nextTabId(), true);
+    } else {
+        const Tab &tab = m_model->activeTab();
+        activatePage(tab.tabId(), true);
     }
 }
 