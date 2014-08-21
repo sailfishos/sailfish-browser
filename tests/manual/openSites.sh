f475f6f9e399a4c4c363f4e765e94bd55c0178a9
Vesa-Matti Hartikainen <vesa-matti.hartikainen@jollamobile.com>
2014-08-21 17:24:27 +0300

    change openSites test script to add sites as tabs in history db instead of opening them

--- tests/manual/openSites.sh
+++ tests/manual/openSites.sh
@@ -8,6 +8,7 @@ fi
 echo "Opening sites from $SITES"
 
 for line in `cat $SITES`; do
-  xdg-open "http://$line"
-  sleep 5
+  echo "Adding $line"
+  sqlite3 /home/nemo/.local/share/org.sailfishos/sailfish-browser/sailfish-browser.sqlite "insert into link (url) values (\"http://$line\");"
+  sqlite3 /home/nemo/.local/share/org.sailfishos/sailfish-browser/sailfish-browser.sqlite "insert into tab_history (tab_id, link_id) values ((select max(tab_id)+1 from tab), (select max(link_id) from link)); insert into tab (tab_id, tab_history_id) values ((select max(tab_id)+1 from tab), (select max(id) from tab_history));"
 done
