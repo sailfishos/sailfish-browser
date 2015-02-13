d64ee25a11859b825de9486856eb2644a4fbd618
Siteshwar Vashisht <siteshwar@gmail.com>
2015-02-13 20:53:25 +0530

    Increase tab id start range for private tab model

--- src/privatetabmodel.cpp
+++ src/privatetabmodel.cpp
@@ -14,7 +14,7 @@
 #include "dbmanager.h"
 
 PrivateTabModel::PrivateTabModel(QObject *parent)
-    : DeclarativeTabModel(DBManager::instance()->getMaxTabId() + 1000, parent),
+    : DeclarativeTabModel(DBManager::instance()->getMaxTabId() + 10000, parent),
       m_nextLinkId(1)
 {
     // Startup should be synced to this.
