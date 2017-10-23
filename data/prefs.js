43d4e3051b8c080fce57f89d751e03f2b6fb549f
Raine Makelainen <raine.makelainen@jolla.com>
2017-10-23 16:29:09 +0300

    [sailfish-browser] Write rendering preferences to prefs.js. Contributes to JB#39851
    This makes it sure that preferences are available for the Gecko compositor
    thread when it requests gl context.

--- data/prefs.js
+++ data/prefs.js
@@ -1,1 +1,4 @@
 user_pref("general.useragent.updates.enabled", true);
+user_pref("gfx.compositor.external-window", true);
+user_pref("gfx.compositor.clear-context", false);
+user_pref("embedlite.compositor.external_gl_context", true);
