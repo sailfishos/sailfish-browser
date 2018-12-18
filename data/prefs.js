b305364df9af67fb39648d88ce2adc59d40cfc39
Raine Makelainen <raine.makelainen@jolla.com>
2018-12-18 17:45:52 +0200

    Fix default preferences

--- data/prefs.js
+++ data/prefs.js
@@ -2,3 +2,5 @@ user_pref("general.useragent.updates.enabled", true);
 user_pref("gfx.compositor.external-window", true);
 user_pref("gfx.compositor.clear-context", false);
 user_pref("embedlite.compositor.external_gl_context", true);
+user_pref("apz.allow_zooming", true);
+user_pref("dom.meta-viewport.enabled", true);
