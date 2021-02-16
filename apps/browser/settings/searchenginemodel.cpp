58fa343a52edc90b59a688c8bd2d1af4926c44e7
Marko Kenttala <marko.kenttala@jolla.com>
2021-02-16 13:08:17 +0200

    [browser] Support for removing opensearch engines. Fixes JB#53038

--- apps/browser/settings/searchenginemodel.cpp
+++ apps/browser/settings/searchenginemodel.cpp
@@ -13,7 +13,10 @@
 #include "../bookmarks/datafetcher.h"
 
 #include <QString>
+#include <QFile>
+
 #include <MGConfItem>
+#include <webengine.h>
 
 const auto SEARCH_ENGINE_CONFIG = QStringLiteral("/apps/sailfish-browser/settings/search_engine");
 const auto SEARCH_ENGINES_AVAILABLE_CONFIG = QStringLiteral("/apps/sailfish-browser/settings/search_engines_available");
@@ -21,16 +24,24 @@ const auto SEARCH_ENGINES_AVAILABLE_CONFIG = QStringLiteral("/apps/sailfish-brow
 SearchEngineModel::SearchEngineModel(QObject *parent)
     : QAbstractListModel(parent)
 {
-    QStringList searchEngineList = OpenSearchConfigs::getSearchEngineList();
-    for (const QString &name : searchEngineList) {
-        SearchEngine engine(QUrl(), name, true);
+    QString userSearchPrefix = OpenSearchConfigs::getOpenSearchConfigPath();
+    QMap<QString, QString> searchConfigs = OpenSearchConfigs::getAvailableOpenSearchConfigs();
+    Status status;
+
+    for (const QString &name : searchConfigs.keys()) {
+        // User installed searches are saved to user config dir
+        if (searchConfigs.value(name).startsWith(userSearchPrefix)) status = Status::UserInstalled;
+        else status = Status::System;
+
+        SearchEngine engine(QUrl(), name, status);
         m_searchEngines.append(engine);
     }
 
+    // Get available searches from config
     MGConfItem gconf(SEARCH_ENGINES_AVAILABLE_CONFIG);
     QMap<QString, QVariant> engines = gconf.value().toMap();
     for (const QString &name : engines.keys()) {
-        SearchEngine engine(engines.value(name).toString(), name, false);
+        SearchEngine engine(engines.value(name).toString(), name, Status::Available);
         m_searchEngines.append(engine);
     }
 }
@@ -57,8 +68,8 @@ QVariant SearchEngineModel::data(const QModelIndex &index, int role) const
         return searchEngine.url;
     case TitleRole:
         return searchEngine.title;
-    case InstalledRole:
-        return searchEngine.installed;
+    case StatusRole:
+        return searchEngine.status;
     default:
         return QVariant();
     }
@@ -69,7 +80,7 @@ QHash<int, QByteArray> SearchEngineModel::roleNames() const
     QHash<int, QByteArray> roles;
     roles[UrlRole] = "url";
     roles[TitleRole] = "title";
-    roles[InstalledRole] = "installed";
+    roles[StatusRole] = "status";
     return roles;
 }
 
@@ -83,14 +94,14 @@ void SearchEngineModel::componentComplete()
 
 }
 
-void SearchEngineModel::add(const QString& title, const QString& url)
+void SearchEngineModel::add(const QString &title, const QString &url)
 {
     for (const SearchEngine& engine : m_searchEngines) {
         if (engine.title == title) {
             return;
         }
     }
-    SearchEngine engine(url, title, false);
+    SearchEngine engine(url, title, Status::Available);
     beginInsertRows(QModelIndex(), rowCount(), rowCount());
     m_searchEngines.append(engine);
     endInsertRows();
@@ -102,16 +113,16 @@ void SearchEngineModel::add(const QString& title, const QString& url)
     gconf.set(engines);
 }
 
-void SearchEngineModel::install(const QString& title)
+void SearchEngineModel::install(const QString &title)
 {
     for (int i = 0; i < m_searchEngines.count(); i++) {
-        if (m_searchEngines[i].title == title && !m_searchEngines[i].installed) {
+        if (m_searchEngines[i].title == title && m_searchEngines[i].status == Status::Available) {
             DataFetcher *fetcher = new DataFetcher();
             fetcher->setType(DataFetcher::Type::OpenSearch);
             connect(fetcher, &DataFetcher::statusChanged, [this, fetcher, i]() {
                 if (fetcher->status() == DataFetcher::Status::Ready) {
-                    m_searchEngines[i].installed = true;
-                    emit dataChanged(index(i), index(i), QVector<int>() << InstalledRole);
+                    m_searchEngines[i].status = Status::UserInstalled;
+                    emit dataChanged(index(i), index(i), QVector<int>() << StatusRole);
 
                     MGConfItem gconf(SEARCH_ENGINE_CONFIG);
                     gconf.set(m_searchEngines[i].title);
@@ -129,6 +140,35 @@ void SearchEngineModel::install(const QString& title)
                 }
             });
             fetcher->fetch(m_searchEngines[i].url.toString());
+            break;
+        }
+    }
+}
+
+void SearchEngineModel::remove(const QString &title)
+{
+    MGConfItem gconf(SEARCH_ENGINE_CONFIG);
+    if (gconf.value() == title)
+        return;
+
+    for (int i = 0; i < m_searchEngines.count(); i++) {
+        if (m_searchEngines[i].title == title && m_searchEngines[i].status != Status::System) {
+            beginRemoveRows(QModelIndex(), i, i);
+            if (m_searchEngines[i].status == Status::Available) {
+                MGConfItem gconf(SEARCH_ENGINES_AVAILABLE_CONFIG);
+                QMap<QString, QVariant> engines = gconf.value().toMap();
+                engines.remove(title);
+                gconf.set(engines);
+            } else {
+                QFile::remove(OpenSearchConfigs::getAvailableOpenSearchConfigs().value(title));
+                // Inform WebEngine
+                QVariantMap message;
+                message.insert(QLatin1String("msg"), QVariant(QLatin1String("remove")));
+                message.insert(QLatin1String("name"), QVariant(title));
+                SailfishOS::WebEngine::instance()->notifyObservers(QLatin1String("embedui:search"), QVariant(message));
+            }
+            m_searchEngines.removeAt(i);
+            endRemoveRows();
             break;
         }
     }
