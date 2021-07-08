eeaec2c9324affdb644410d2a370f69d3cb9d4ba
David Llewellyn-Jones <david.llewellyn-jones@jolla.com>
2021-07-08 16:56:02 +0000

    [sailfish-browser] Activate multiple available locales. JB#54830 OMP#JOLLA-222
    If the "intl.locale.requested" preference is unset, gecko will default
    to only a single fallback locale. If it's set to an empty string, it
    will instead set the locale based on the OS setting.
    
    This adds the preference as an empty string to the default preferences.

--- data/prefs.js
+++ data/prefs.js
@@ -4,3 +4,4 @@ user_pref("gfx.compositor.clear-context", false);
 user_pref("embedlite.compositor.external_gl_context", true);
 user_pref("apz.allow_zooming", true);
 user_pref("dom.meta-viewport.enabled", true);
+user_pref("intl.locale.requested", "");
